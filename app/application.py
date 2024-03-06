c8303484126a707421d957b4287262bb3b081f7b
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-03-06 15:22:52 +0000

    -removed flask_mail

--- app/application.py
+++ app/application.py
@@ -5,10 +5,15 @@ from config import config
 from scripts.accounts import users
 
 app = Flask(__name__)
-mail = Mail(app)
 app.config['SECRET_KEY'] = config.SECRET_KEY
 app.config['SECURITY_PASSWORD_SALT'] = config.SECURITY_PASSWORD_SALT
 app.config['MAIL_DEFAULT_SENDER'] = config.MAIL_DEFAULT_SENDER
+app.config['MAIL_PORT'] = config.MAIL_PORT
+app.config['MAIL_USE_TLS'] = config.MAIL_USE_TLS
+app.config['MAIL_USE_SSL'] = config.MAIL_USE_SSL
+app.config['MAIL_USERNAME'] = config.MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = config.MAIL_PASSWORD
+mail = Mail(app)
 
 login_manager = LoginManager()
 login_manager.init_app(app)
