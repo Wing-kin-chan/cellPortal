68018053824b7738b0af93c86ad846241a56eb29
Wing-kin-chan <Wing_kin_chan@hotmail.com>
2024-03-06 20:48:46 +0000

    +Routing for profile, confirmation, inactive account

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, request, redirect, url_for, flash
 from flask_login import login_required, login_user, logout_user, current_user
-from scripts.accounts import users, token, email
+from scripts.accounts import users, tokenizer, confirm_email
 from application import app
 
 @app.route('/')
@@ -13,25 +13,48 @@ def explore():
 
 @app.route('/profile', methods = ['GET', 'POST'])
 @login_required
-def profile(user):
-    if not user.isVerified:
+def profile():
+    if not current_user.isVerified:
         return redirect(url_for('inactive'))
     else:
-        return render_template('profile')
+        return render_template('accounts/profile')
 
 @app.route('/confirm/<token>')
 @login_required
-def confirm(token_):
-    try:
-        email = token.confirm_token(token_)
-    except:
-        flash('The confirmation link is invalid or has expired.', 'danger')
-    user = users.User.get_user(email)
-    if user.isVerified:
+def confirm(token):
+    if current_user.isVerified:
         flash('Account has already been confirmed', 'success')
         return redirect(url_for('profile'))
+    user = users.User.get_user(current_user.email)
+    email = tokenizer.confirm_token(token)
+    if user.email == email:
+        users.User.verify(user.email)
+        flash('Email confirmed! You should be redirected to your profile.')
+        return redirect(url_for('profile'))
     else:
-        user.isVerified = True
+        flash('The confirmation link is invalid or has expired.', 'danger')
+        logout_user()
+        return redirect(url_for('index'))
+
+
+@app.route('/inactive')
+@login_required
+def inactive():
+    if current_user.isVerified:
+        return redirect(url_for('index'))
+    return render_template('accounts/inactive.html')
+
+@app.route('/resend')
+@login_required
+def resend():
+    if current_user.isVerified:
+        return redirect(url_for('index'))
+    new_token = tokenizer.generate_token(current_user.email)
+    confirm_url = url_for('confirm', token = new_token, _external = True)
+    html = render_template('accounts/confirm.html', confirm_url = confirm_url)
+    subject = 'cellPortal: Confirm Your Email'
+    confirm_email.send_verification(current_user.email, subject, html)
+    return redirect(url_for('inactive'))
 
 @app.route('/publish', methods = ['GET', 'POST'])
 def publish():
@@ -54,11 +77,16 @@ def publish():
                 return 'Incorrect password.'
         elif action == 'register':
             users.User.create(email, password)
-            new_token = token.generate_token(email)
-            confirm_url = url_for('confirm', token_ = new_token, _external = True)
-            html = render_template('activate.html', confirm_url = confirm_url)
+
+            new_token = tokenizer.generate_token(email)
+            confirm_url = url_for('confirm', token = new_token, _external = True)
+            html = render_template('accounts/confirm.html', confirm_url = confirm_url)
             subject = 'cellPortal: Confirm Your Email'
-            email.send_email(email, subject, html)
+            confirm_email.send_verification(email, subject, html)
+
+            user = users.User.get_user(email)
+            login_user(user)
+            return redirect(url_for('inactive'))
     return render_template('publish.html')
 
 @app.route('/about')
